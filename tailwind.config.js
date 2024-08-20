e3f761fb6ac010cc538b806f4bfcdd6da1d5007a
RobinZeitlin <144109907+RobinZeitlin@users.noreply.github.com>
2024-08-20 14:36:17 +0200

    Feature : Home Page

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,18 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./dist/*.{html,js}"],
+  content: ["./dist/*.{html,js,ts,jsx,tsx,css}"],
   theme: {
-    extend: {},
+    extend: {
+
+      colors:{
+        'primary' : '#15202B',
+        'secondary' : '#253240',
+      },
+
+      fontFamily:{
+        'main-font' : 'Anek Devanagari'
+      }
+    },
   },
   plugins: [],
 }
